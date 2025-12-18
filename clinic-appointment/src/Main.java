39e5f7ea42181b9cadfbea8319ed220047d48fa4
Xaralampos Makridhs <makridhs.xaralampos@gmail.com>
2025-12-18 23:07:49 +0200

    initial commit

--- clinic-appointment/src/Main.java
+++ clinic-appointment/src/Main.java
@@ -1,6 +1,17 @@
+import dbhelper.DBHelper;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello");
         System.out.println("Mak");
+
+        try(Connection conn=DBHelper.getConnection()){
+            System.out.println(conn!=null? "Done":"Fail");
+        }catch (SQLException e){
+            System.out.println("Error"+e.getMessage());
+        }
     }
 }
