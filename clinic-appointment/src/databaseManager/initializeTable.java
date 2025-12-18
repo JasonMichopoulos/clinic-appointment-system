3e9228316fc9f52d565a28f3ebf622ea2c74ca91
Iason Michopoulos <iasonmixopoulos1@gmail.com>
2025-12-19 00:24:55 +0200

    initial commit

--- clinic-appointment/src/databaseManager/initializeTable.java
+++ clinic-appointment/src/databaseManager/initializeTable.java
@@ -7,14 +7,16 @@ import java.sql.Statement;
 public class initializeTable {
     public static void createTables() throws SQLException {
         String sql = """
-                CREATE TABLE IF NOT EXISTS patients(
-                        id INT AUTO_INCREMENT PRIMARY KEY,
-                        patient_id INT,
-                        first_name VARCHAR(255) NOT NULL,
-                        last_name VARCHAR(255) NOT NULL,
-                        phone_number VARCHAR(10) NOT NULL UNIQUE,
-                        sector VARCHAR(255) NOT NULL,
-                        FOREIGN KEY (patient_id) REFERENCES patients(id) ON DELETE CASCADE
+                CREATE TABLE patients(
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                first_name VARCHAR(255) NOT NULL,
+                last_name VARCHAR(255) NOT NULL,
+                father_name VARCHAR(255),
+                amka VARCHAR(11) NOT NULL UNIQUE,
+                phone_number VARCHAR(10) NOT NULL UNIQUE,
+                emergency_call VARCHAR(10),
+                address VARCHAR(255) NOT NULL,
+                notes TEXT
                 );
                 """;
         try (Connection conn = DataBaseConnect.getConnection();
@@ -33,6 +35,7 @@ public class initializeTable {
                     doctor_id INT,
                     appointment_date_start DATETIME NOT NULL,
                     appointment_date_end DATETIME NOT NULL,
+                    is_done BOOLEAN DEFAULT FALSE,
                     FOREIGN KEY (patient_id) REFERENCES patients(id) ON DELETE CASCADE,
                     FOREIGN KEY (doctor_id) REFERENCES doctors(id) ON DELETE CASCADE
                 );
